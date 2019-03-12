29951a8738e8f8cb09c3778c6fb61e13baf56f85
Koen van Gilst <koen.vangilst@gmail.com>
2019-03-12 19:33:01 +0100

    even better hooks

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import ReactDOM from 'react-dom'
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 
-import { draw } from './utils'
-import { usePersistantState } from './hooks'
+import { usePersistentCanvas } from './hooks'
 import './styles.css'
 
 function App() {
-  const [locations, setLocations] = usePersistantState([])
-  const canvasRef = useRef(null)
+  const [locations, setLocations, canvasRef] = usePersistentCanvas([])
 
   const width = window.innerWidth
   const height = window.innerHeight
   const style = { width, height }
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-    ctx.clearRect(0, 0, height, width)
-    locations.forEach(location => draw(ctx, location))
-  })
 
   function handleDraw(e) {
     setLocations([...locations, { x: e.clientX, y: e.clientY }])
