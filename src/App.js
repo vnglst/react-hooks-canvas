b0cf0274c7ba6c79b15704ad52eaa62c0731a468
Koen van Gilst <koen.vangilst@gmail.com>
2019-03-15 10:28:31 +0100

    reverting changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
 
-import { draw } from './hooks'
+import { usePersistentCanvas } from './hooks'
 import './App.css'
 
 function App() {
-  const [locations, setLocations] = React.useState([])
-  const canvasRef = React.useRef(null)
+  const [locations, setLocations, canvasRef] = usePersistentCanvas()
 
-  React.useEffect(() => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-    ctx.clearRect(0, 0, window.innerHeight, window.innerWidth)
-    locations.forEach(location => draw(ctx, location))
-  })
-
-  function handleCanvasClick(e) {
-    const newLocation = { x: e.clientX, y: e.clientY }
-    setLocations([...locations, newLocation])
+  function handleDraw(e) {
+    setLocations([...locations, { x: e.clientX, y: e.clientY }])
   }
 
   function handleClear() {
@@ -24,18 +15,20 @@ function App() {
   }
 
   function handleUndo() {
-    setLocations(locations.slice(0, locations.length - 1))
+    setLocations(locations.slice(0, -1))
   }
 
   return (
     <>
-      <button onClick={handleClear}>Clear</button>
-      <button onClick={handleUndo}>Undo</button>
+      <div className="controls">
+        <button onClick={handleClear}>Clear</button>
+        <button onClick={handleUndo}>Undo</button>
+      </div>
       <canvas
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight}
-        onClick={handleCanvasClick}
+        onClick={handleDraw}
       />
     </>
   )
